9cacf28d2f9e4dc061fc47cedc3d3dae4711cbba
SotaSuzuki <3.3sota.suzuki@gmail.com>
2017-12-02 17:13:12 +0900

    Remove MyButton compoennts from config.js

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,11 +3,7 @@ import { configure } from '@storybook/vue'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import MyButton from './../src/components/MyButton.vue'
-
 Vue.use(Vuex)
-
-Vue.component('MyButton', MyButton)
 
 function loadStories () {
   require('./../src/stories')
